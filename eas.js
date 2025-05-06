09927b66f018dd2d448af4ad22c15f038cfc1b0e
Aditya <ranjanaditya452@gmail.com>
2025-05-06 14:32:21 +0530

    Done with etch a sketch , edit button added

--- eas.js
+++ eas.js
@@ -1,11 +1,47 @@
 let mainCont = document.querySelector(".mainContainer");
-
-for (let i=0;i<16;i++)
+mainCont.style.display="flex";
+let choice=16;
+function createGrid(choice){
+for (let i=0;i<choice;i++)
 { let smallCont = document.createElement("div");
-    for (let j=0;j<16;j++)
+    for (let j=0;j<choice;j++)
     {
         let smallDiv = document.createElement("div");
-        smallCont.append(smallDiv);
+        smallDiv.style.width= 800/choice +"px";
+        smallDiv.style.height= 800/choice + "px";
+        smallDiv.style.border= "1px solid black";
+        smallCont.appendChild(smallDiv);
+
+        smallDiv.addEventListener('mouseover',(event)=>
+        {
+            smallDiv.style.background=colorMe();
+        })
+
     }
-    mainCont.append(smallCont);
+    mainCont.appendChild(smallCont);
 }
+}
+function colorMe(){
+    let ran = Math.floor(Math.random() * 3);
+
+    if(ran===0)
+    {
+        return "red";
+    }
+    else if (ran ===1)
+    {
+        return "blue";
+    }
+    else if (ran===2)
+    {
+        return "green";
+    }
+
+}
+createGrid(choice);
+let resButton = document.querySelector(".btn");
+resButton.addEventListener('click',(event)=>
+{   mainCont.innerHTML= "";
+    let cchoice = prompt("No of squares per side:");
+    createGrid(cchoice);
+})
